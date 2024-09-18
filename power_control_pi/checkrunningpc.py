012bef4ed6f212b29605f50e4d502bb35c31930f
Lennard Kießling <lennard-2@web.de>
2024-09-18 10:05:25 +0200

    changed outdated variables

--- power_control_pi/checkrunningpc.py
+++ power_control_pi/checkrunningpc.py
@@ -1,19 +1,19 @@
 import time
 import gpiod
 
-BUTTON_PIN = 27
+LED_ON_PIN = 27
 chip = gpiod.Chip('gpiochip4')
-button_line = chip.get_line(BUTTON_PIN)
-button_line.release()
-button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
+led_line = chip.get_line(LED_ON_PIN)
+led_line.release()
+led_line.request(consumer="Transistor", type=gpiod.LINE_REQ_DIR_IN)
 
 try:
    while True:
-       button_state = button_line.get_value()
+       pc_state = led_line.get_value()
        time.sleep(5)
-       if button_state == 1:
+       if pc_state == 1:
            print('on')
        else:
            print('off')
 finally:
-    button_line.release()
+    led_line.release()
